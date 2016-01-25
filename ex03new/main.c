e1348cd6107a2cdada1633f925be88c2992753d5
Huber Nicolas <nicolas@Hubers-MacBook-Pro.local>
2016-01-25 14:53:56 +0100

    .swp bugg fixed

--- ex03new/main.c
+++ ex03new/main.c
@@ -6,7 +6,7 @@
 /*   By: nhuber <nhuber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/21 14:11:38 by nhuber            #+#    #+#             */
-/*   Updated: 2016/01/22 23:52:13 by nhuber           ###   ########.fr       */
+/*   Updated: 2016/01/25 14:53:35 by nhuber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ int	main(int ac, char **av)
 	while (i < ac)
 	{
 		ft_create_list(av[i], &head);
-		ft_putstr(head->buffer);
 		i++;
 	}
 	return (0);
