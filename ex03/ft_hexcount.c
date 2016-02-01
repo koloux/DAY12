22f29225644eccfab4b6f2b817041dc3e2e0e6e8
Huber Nicolas <nicolas@Hubers-MacBook-Pro.local>
2016-02-01 23:46:14 +0100

    CorrectedHex

--- ex03/ft_hexcount.c
+++ ex03/ft_hexcount.c
@@ -6,7 +6,7 @@
 /*   By: nhuber <nhuber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/18 16:07:55 by nhuber            #+#    #+#             */
-/*   Updated: 2016/02/01 20:14:30 by nhuber           ###   ########.fr       */
+/*   Updated: 2016/02/01 23:45:08 by nhuber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 void	ft_print_hexcount(int n)
 {
 	char	*hexcount;
-	int		len;
+	int	len;
 
 	hexcount = ft_itoa_base(n, 16);
-	if ((len = ft_strlen(hexcount)) < 9)
+	len = ft_strlen(hexcount);
+	while (len < 7)
 	{
-		while (len < 8)
-		{
-			ft_putnbr(0);
-			len++;
-		}
+		ft_putnbr(0);
+		len++;
 	}
 	ft_putstr(hexcount);
 }
